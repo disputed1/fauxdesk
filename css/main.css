be3038b1b0f7faf0e429f677548b4719b87db91d
J.Mac <jmcnab1990@gmail.com>
2017-04-24 07:38:02 -0400

    Add files via upload

---
+++ css/main.css
@@ -0,0 +1,113 @@
+// TODO - Handle when lines have to wrap
+body {
+  font-size: 16px;
+  background-image: kali.png no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.terminal-window {
+  background-color: #000;
+  color: #fff;
+  border: outset;
+  border-style: solid;
+  border-color: #d3d3d3;
+  font-family: -apple-system, "BlinkMacSystemFont", sans-serif;
+  cursor: text;
+  position:relative;
+  width: 60%;
+  top: 5%;
+  left: 20%;
+}
+
+.title-bar {
+  background: #e0e0e0;
+  background: linear-gradient(#f0f0f0 , #d3d3d3);
+  border-top-left-radius: .3em;
+  border-top-right-radius: .3em;
+  color: #000;
+  font-family: -apple-system, "BlinkMacSystemFont", sans-serif;
+  overflow: hidden;
+  cursor: move;
+  padding: .4em;
+  margin: -.2em -.1em 0em -.1em;
+}
+/*------------- Enviromental Variables ------------ */
+
+p {
+	font-family:monospace;
+}
+
+input {
+  width:80%;
+  length:80%;  
+  margin: 0px 3px;
+  border: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+
+textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+}
+
+/* ------------------------------------------------ */
+
+/*------------- Icon Bullshit --------------------- */
+
+.icons {
+  color:#fff;
+  font-family:monospace;
+  max-width:60px;
+  margin-left:2%;
+}
+
+.caption {
+  color: #fff;
+  font-family: monospace;
+  margin: auto;
+  cursor: pointer;
+}
+	
+.icons:hover{
+background: blue; /* make this whatever you want */
+}
+
+/* ------------------------------------------------ */
+
+.title-bar div {
+  text-align: center;
+/*  cursor:move;
+  position:relative; /* important (all position that's not `static`) */
+}
+
+.title-button {
+  border: 1px solid;
+  border-radius: 50%;
+  width: 1em;
+  height: 1em;
+  float: right;
+  margin: .1875em;
+  cursor:pointer;
+}
+
+.terminal-content {
+  border: hidden;
+  padding: 0px 0px 0px 0px;
+  background-color: #000;
+  font-family: monospace;
+  width: 100%;
+  color: #fff;
+  font-size: 1em;
+}
+
